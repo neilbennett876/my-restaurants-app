95c6426cbf3f732419ce49dbcc4e64f925f967a2
Neil <neil.bennett01@gmail.com>
2022-05-25 15:18:07 -0400

    connected to restaurant api

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState()
+
+
+useEffect(async () => {
+  const getData = async () => {
+    try {
+  
+   const response = await fetch('https://my-first-firestore-nb.web.app/restaurants')
+   const data = await response.json()
+   setAllRestaurants(data)
+} catch (err) {
+  console.error(err)
+} 
+  }
+
+  getData()
+},[])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {allRestaurants?.map(res => <Text key={res.id}>{res.name}</Text>)}
+      <Text>Do you refresh?</Text>
       <StatusBar style="auto" />
     </View>
   );
