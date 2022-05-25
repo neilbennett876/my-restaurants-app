4c89b5cabaaf04026240a42ed75f194d46330c38
Neil <neil.bennett01@gmail.com>
2022-05-25 16:15:34 -0400

    added background photo

--- App.js
+++ App.js
@@ -1,41 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, ScrollView, Image } from "react-native";
+import { useEffect, useState } from "react";
+
+
+const image = {
+  uri: "https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg"
+};
 
 export default function App() {
-  const [allRestaurants, setAllRestaurants] = useState()
+  const [allRestaurants, setAllRestaurants] = useState();
+  useEffect(async () => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://my-first-firestore-nb.web.app/restaurants"
+        );
+        const data = await response.json();
+        setAllRestaurants(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
 
-
-useEffect(async () => {
-  const getData = async () => {
-    try {
-  
-   const response = await fetch('https://my-first-firestore-nb.web.app/restaurants')
-   const data = await response.json()
-   setAllRestaurants(data)
-} catch (err) {
-  console.error(err)
-} 
-  }
-
-  getData()
-},[])
-  
   return (
     <View style={styles.container}>
-      {allRestaurants?.map(res => <Text key={res.id}>{res.name}</Text>)}
-      <Text>Do you refresh?</Text>
-      <StatusBar style="auto" />
+      <ImageBackground resizeMode="cover" source={image} style={styles.container}>
+        <ScrollView>
+        {allRestaurants ? (
+          allRestaurants?.map((res) => <Text style={styles.restaurantName} key={res.id}>{res.name}</Text>)
+          ) : (
+            <ActivityIndicator size="large" color="orange" />
+            )}
+        <Text>Do you refresh?</Text>
+        <StatusBar style="auto" />
+            </ScrollView>
+            </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  restaurantName: {
+    color: 'white',
+    fontSize: 70,
+  }
 });
